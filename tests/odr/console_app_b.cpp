d1cffeffa5dd37fb7c9b67505652c6b56867248c
Anastasia Prokop <gpae98@gmail.com>
2025-11-04 15:50:41 +0300

    fix yaml files

--- tests/odr/console_app_b.cpp
+++ tests/odr/console_app_b.cpp
@@ -1,4 +1,4 @@
-#include <consolix/core/ConsoleApplication.hpp>
+#include "../../include/consolix/core/ConsoleApplication.hpp"
 
 extern "C" consolix::ConsoleApplication* get_console_app_b() { 
 	return std::addressof(consolix::ConsoleApplication::get_instance()); 
