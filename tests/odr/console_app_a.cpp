42fb3bff37092b0ff7228c8e018ebb45e9bd658f
Anastasia Prokop <gpae98@gmail.com>
2025-11-04 15:35:53 +0300

    add console app test files

---
+++ tests/odr/console_app_a.cpp
@@ -0,0 +1,5 @@
+#include <consolix/core/ConsoleApplication.hpp>
+
+extern "C" consolix::ConsoleApplication* get_console_app_a() { 
+	return std::addressof(consolix::ConsoleApplication::get_instance()); 
+}
---
+++ tests/odr/console_app_b.cpp
@@ -0,0 +1,5 @@
+#include <consolix/core/ConsoleApplication.hpp>
+
+extern "C" consolix::ConsoleApplication* get_console_app_b() { 
+	return std::addressof(consolix::ConsoleApplication::get_instance()); 
+}
