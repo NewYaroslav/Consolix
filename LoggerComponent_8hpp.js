03c9bf184b7c0e9400ad6b265eb0ba3031c44cf3
NewYaroslav <NewYaroslav@users.noreply.github.com>
2026-06-03 03:21:25 +0000

    Publish 'docs/html' to 'gh-pages' in 'NewYaroslav/Consolix'

--- LoggerComponent_8hpp.js
+++ LoggerComponent_8hpp.js
@@ -2,6 +2,8 @@ var LoggerComponent_8hpp =
 [
     [ "consolix::LoggerComponent", "classconsolix_1_1LoggerComponent.html", "classconsolix_1_1LoggerComponent" ],
     [ "_CONSOLIX_LOGGER_COMPONENT_HPP_INCLUDED", "LoggerComponent_8hpp.html#af22a15b0dd2e4f159e1992815351977e", null ],
+    [ "CONSOLIX_LOG_STREAM", "LoggerComponent_8hpp.html#a5deca44aa81f53db45becadaa33a8886", null ],
+    [ "CONSOLIX_LOG_STREAM_EX", "LoggerComponent_8hpp.html#a48ad32c348f9563e53a572bae19748d7", null ],
     [ "CONSOLIX_LOGO_STREAM", "LoggerComponent_8hpp.html#afe67f762cd2ce2e8eb51b0a77579ab4a", null ],
     [ "CONSOLIX_STREAM", "LoggerComponent_8hpp.html#a188cf759d4bc30f22194e6ab319df750", null ]
 ];
